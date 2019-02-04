d8a6d945cdb9a574d8f162f1130d3f65fbda7e5d
wilfrank84 <wilfrank84@gmail.com>
2019-02-04 00:43:44 -0200

    add angular 2 token package

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import templateString from './app.component.html'
 import styleString from './app.component.scss';
 
+import { Angular2TokenService } from 'angular2-token';
+
 @Component({
   selector: 'hello-angular',
   template: templateString,
@@ -12,7 +14,20 @@ import styleString from './app.component.scss';
 export class AppComponent {
   name = 'Angular';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private tokenService: Angular2TokenService
+  ) {
+    this.tokenService.init({
+      apiBase: 'http://localhost:3000',
+      resetPasswordCallback: window.location.href,
+      globalOptions: {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    })
+  }
 
   public changeName() {
     this.http.get('/hello_angular/name').subscribe(data => {
--- app/javascript/hello_angular/app/app.module.ts
+++ app/javascript/hello_angular/app/app.module.ts
@@ -1,8 +1,13 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// Angular Plugins Imports
+import { Angular2TokenService } from 'angular2-token';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    Angular2TokenService,
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
