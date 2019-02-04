8b890dc9c58a1699304a45ed4374123112e583d0
wilfrank84 <wilfrank84@gmail.com>
2019-02-04 00:23:34 -0200

    add adjustments to devise token auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', defaults: { format: :json }
 
   root 'hello_angular#index'
+
   get 'hello_angular/index'
   get 'hello_angular/name'
+
+  resources :users, only: [:index]
 end
